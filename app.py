a1e610b832227d3baad4861e6008626fcd4a7a4d
pratixashah <shah.pratixa@gmail.com>
2021-03-11 23:06:33 -0800

    Mongodb Set up with Flask

--- app.py
+++ app.py
@@ -4,14 +4,15 @@ from bs4 import BeautifulSoup as bs
 import requests
 import time
 import pandas as pd
+from flask import Response
+from flask import Flask, jsonify
+from flask import Flask, render_template, redirect
+from flask_pymongo import PyMongo
 
 from splinter import Browser
 from webdriver_manager.chrome import ChromeDriverManager
 
-from flask import Response
-from flask import Flask, jsonify
-
-from scrape_mars import scrape
+import scrape_mars
 #################################################
 # Flask Setup
 #################################################
@@ -19,20 +20,23 @@ app = Flask(__name__)
 #################################################
 # Flask Routes
 #################################################
+# Use flask_pymongo to set up mongo connection
+app.config["MONGO_URI"] = "mongodb://localhost:27017/mars_app"
+mongo = PyMongo(app)
 
 @app.route("/")
-def welcome():
-    """List all available api routes."""
-    return (
-        f"Available Routes:<br/>"
-        f"/<br/>"
-        f"/scrape<br/>"
-    )
+def index():
+    mars_data = mongo.db.collection.find_one()
+    return render_template("index.html", mars=mars_data)
 
 @app.route("/scrape")
-def scrape_data():
-    return scrape()
+def scrape():
+    mars_data = scrape_mars.scrape()
+    # Update the Mongo database using update and upsert=True
+    mongo.db.collection.update({}, mars_data, upsert=True)
 
+    # Redirect back to home page
+    return redirect("/", code=302)
 
 if __name__ == '__main__':
     app.run(debug=True)
